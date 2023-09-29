ea5b8afe1f7ddd106a1ac9a9cbbb62a08eaa99b6
svkjanco <svkjanco@gmail.com>
2023-09-29 11:49:18 +0200

    fixing problems

--- models.py
+++ models.py
@@ -23,6 +23,22 @@ login_model = api.model("Login Data", {
     "password": fields.String(required=True, description="User password")
 })
 
+question_rating_model = api.model("Question Rating", {
+    "question_id": fields.String,
+    "question": fields.String,
+    "rating": fields.Integer,
+    "category": fields.String,
+    "icon": fields.String
+})
+
+rating_model = api.model("Rating", {
+    "date_rated": fields.Date,
+    "rated_by_user_id": fields.String,
+    "client_id": fields.String,
+    "phase_no": fields.Integer,
+    "questions_rating": fields.List(fields.Nested(question_rating_model))
+})
+
 client_model = api.model("Add client", {
     "name": fields.String,
     "surname": fields.String,
